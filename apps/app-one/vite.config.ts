bdcd55d94f0d0ac6d5c1c964f8b20123247c721c
aldosch <git@aldo.io>
2024-01-22 12:55:07 +1100

    try appType mpa

--- apps/app-one/vite.config.ts
+++ apps/app-one/vite.config.ts
@@ -5,6 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
+  appType: 'mpa',
   cacheDir: '../../node_modules/.vite/app-one',
 
   server: {
